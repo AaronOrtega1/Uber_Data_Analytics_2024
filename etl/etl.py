06a70278cb72599ae7ad31ff645a60013512ea7b
AaronOrtega1 <aaron.ortega15@outlook.es>
2025-09-17 19:12:43 -0600

    python etl to merge date and time col into timestamp col added required libraries to connect to sql db

--- etl/etl.py
+++ etl/etl.py
@@ -1,4 +1,8 @@
+import os
+
 import pandas as pd
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
 
 
 class ETL:
@@ -11,16 +15,42 @@ class ETL:
         """
         self.data = data
 
+    def merge_date_time_to_timestamp(
+        self, col_date: str, col_time: str, col_timestamp: str = "timestamp"
+    ):
+        """
+        Converts a date column and a time column in the DataFrame to datetime objects in a new column.
+
+        Args:
+                col_date (str): Name of the date column.
+                col_time (str): Name of the time column.
+                col_timestamp (str): Desired name of the timestamp column. (Default: timestamp)
+        """
+        # Check if columns exist
+        if col_date not in self.data.columns:
+            raise KeyError(f"Column '{col_date}' not found in DataFrame")
+        if col_time not in self.data.columns:
+            raise KeyError(f"Column '{col_time}' not found in DataFrame")
+
+        # Merge Date + Time into single timestamp
+        self.data[col_timestamp] = pd.to_datetime(
+            self.data[col_date] + " " + self.data[col_time]
+        )
+
+        return self.data
+
 
 def main():
     input_path = "/data/ncr_ride_bookings.csv"
-    output_path = "/data/ncr_ride_bookings_transformed.csv"
+
     # Read CSV
     df = pd.read_csv(input_path)
+
+    # Initialize the etl class with the df as data
     etl = ETL(df)
-    # Write CSV
-    df.to_csv(output_path, index=False)
-    print(f"Saved transformed csv to {output_path}")
+
+    # Merge Date and Time cols into booking_timestamp col
+    df = etl.merge_data_time_to_timestamp("Date", "Time", "booking_timestamp")
 
 
 if __name__ == "__main__":
